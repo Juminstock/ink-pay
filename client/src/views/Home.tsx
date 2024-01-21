99621df3fd83870c21078593dcdd4f27187ddd2c
Carlos Rodríguez <juminstock@gmail.com>
2024-01-21 01:40:33 -0500

    Update: Create home view and added nextui packages

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, Spinner, getKeyValue} from "@nextui-org/react";
+import {useInfiniteScroll} from "@nextui-org/use-infinite-scroll";
+import {useAsyncList} from "@react-stately/data";
 
-function Home () {
-  return <div>Home</div>
+function Home() {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [hasMore, setHasMore] = React.useState(false);
+
+  let list = useAsyncList({
+    async load({signal, cursor}) {
+
+      if (cursor) {
+        setIsLoading(false);
+      }
+
+      // If no cursor is available, then we're loading the first page.
+      // Otherwise, the cursor is the next URL to load, as returned from the previous page.
+      const res = await fetch(cursor || "https://swapi.py4e.com/api/people/?search=", {signal});
+      let json = await res.json();
+
+      setHasMore(json.next !== null);
+
+      return {
+        items: json.results,
+        cursor: json.next,
+      };
+    },
+  });
+
+  const [loaderRef, scrollerRef] = useInfiniteScroll({hasMore, onLoadMore: list.loadMore});
+
+  return (
+    <Table
+      isHeaderSticky
+      aria-label="Órdenes top"
+      baseRef={scrollerRef}
+      bottomContent={
+        hasMore ? (
+          <div className="flex w-full justify-center">
+            <Spinner ref={loaderRef} color="primary" />
+          </div>
+        ) : null
+      }
+      classNames={{
+        base: "max-h-[820px] overflow-scroll",
+        table: "min-h-[600px]",
+      }}
+    >
+      <TableHeader>
+        <TableColumn key="name">PRECIO</TableColumn>
+        <TableColumn key="height">DISPONIBLE</TableColumn>
+        <TableColumn key="mass">VENDEDOR</TableColumn>
+        <TableColumn key="birth_year">ÓRDENES EXITOSAS</TableColumn>
+      </TableHeader>
+      <TableBody
+        isLoading={isLoading}
+        items={list.items}
+        loadingContent={<Spinner color="black" />}
+      >
+        {(item) => (
+          <TableRow key={item.name}>
+            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
 }
 
 export { Home }
