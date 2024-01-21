c8db638a862de8c10a5c427de7332df08629e9f4
Another <joseluismanco37@gmail.com>
2024-01-21 02:11:21 -0500

    feat: add functionality to table

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,71 +1,198 @@
-import React from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, Spinner, getKeyValue} from "@nextui-org/react";
-import {useInfiniteScroll} from "@nextui-org/use-infinite-scroll";
-import {useAsyncList} from "@react-stately/data";
+import React from 'react'
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Pagination,
+  Spinner,
+  getKeyValue,
+  Card,
+  CardBody,
+  Button
+} from '@nextui-org/react'
+import { useAsyncList } from '@react-stately/data'
+import { useNavigate } from 'react-router-dom'
 
-function Home() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [hasMore, setHasMore] = React.useState(false);
+const mockData = [
+  {
+    id: 1,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 2,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 3,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 4,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 5,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 6,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 7,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 8,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 9,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 10,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 11,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  },
+  {
+    id: 12,
+    depositor: '0x34107Bce5EC357cE31739B84454c2c555a677568',
+    depositAmount: 100,
+    remainingDeposits: 78,
+    conversionRate: 17.75,
+    outstandingIntentAmount: 70
+  }
+]
 
-  let list = useAsyncList({
-    async load({signal, cursor}) {
+function Home () {
+  const [page, setPage] = React.useState(1)
+  const rowsPerPage = 10
+  const navigate = useNavigate()
 
-      if (cursor) {
-        setIsLoading(false);
-      }
+  const pages = Math.ceil(mockData.length / rowsPerPage)
 
-      // If no cursor is available, then we're loading the first page.
-      // Otherwise, the cursor is the next URL to load, as returned from the previous page.
-      const res = await fetch(cursor || "https://swapi.py4e.com/api/people/?search=", {signal});
-      let json = await res.json();
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage
+    const end = start + rowsPerPage
 
-      setHasMore(json.next !== null);
+    return mockData.slice(start, end)
+  }, [page, mockData])
 
-      return {
-        items: json.results,
-        cursor: json.next,
-      };
-    },
-  });
-
-  const [loaderRef, scrollerRef] = useInfiniteScroll({hasMore, onLoadMore: list.loadMore});
+  const handleOnClickButton = (id: number) => {
+    navigate(`/make-intent/${id}`)
+  }
 
   return (
-    <Table
-      isHeaderSticky
-      aria-label="Órdenes top"
-      baseRef={scrollerRef}
-      bottomContent={
-        hasMore ? (
-          <div className="flex w-full justify-center">
-            <Spinner ref={loaderRef} color="primary" />
+    <main className='flex flex-col justify-center min-h-screen gap-5 items-center'>
+      <Table
+        className='w-[80vw]'
+        isHeaderSticky
+        aria-label='Órdenes top'
+        bottomContent={
+          <div className='flex w-full justify-center'>
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              color='secondary'
+              page={page}
+              total={pages}
+              onChange={page => setPage(page)}
+            />
           </div>
-        ) : null
-      }
-      classNames={{
-        base: "max-h-[820px] overflow-scroll",
-        table: "min-h-[600px]",
-      }}
-    >
-      <TableHeader>
-        <TableColumn key="name">PRECIO</TableColumn>
-        <TableColumn key="height">DISPONIBLE</TableColumn>
-        <TableColumn key="mass">VENDEDOR</TableColumn>
-        <TableColumn key="birth_year">ÓRDENES EXITOSAS</TableColumn>
-      </TableHeader>
-      <TableBody
-        isLoading={isLoading}
-        items={list.items}
-        loadingContent={<Spinner color="black" />}
+        }
+        classNames={{
+          base: 'max-h-[820px] overflow-scroll',
+          table: 'max-h-[60vh]',
+          th: 'bg-secondary text-white font-bold',
+          wrapper: ' bg-card-bg'
+        }}
       >
-        {(item) => (
-          <TableRow key={item.name}>
-            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  );
+        <TableHeader>
+          <TableColumn key='conversionRate'>PRECIO</TableColumn>
+          <TableColumn key='remainingDeposits'>DISPONIBLE</TableColumn>
+          <TableColumn key='depositor'>VENDEDOR</TableColumn>
+          <TableColumn key='outstandingIntentAmount'>
+            ÓRDENES EXITOSAS
+          </TableColumn>
+          <TableColumn key='action'>ACCIÓN</TableColumn>
+        </TableHeader>
+        <TableBody items={items} loadingContent={<Spinner color='black' />}>
+          {item => (
+            <TableRow key={item.id} accessKey={item.id.toString()}>
+              {columnKey => {
+                if (columnKey === 'action') {
+                  return (
+                    <TableCell>
+                      <Button
+                        onClick={() => handleOnClickButton(item.id)}
+                        className='bg-main text-white font-bold'
+                      >
+                        Comprar
+                      </Button>
+                    </TableCell>
+                  )
+                }
+
+                return <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+              }}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </main>
+  )
 }
 
 export { Home }
